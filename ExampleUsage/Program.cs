02243ccce0300d88a5ad5e865a855bdf0e4f3528
Martin Halliday <martin.bikeman@gmail.com>
2016-06-08 22:17:39 -0700

    Write exceptions ot the console window

--- ExampleUsage/Program.cs
+++ ExampleUsage/Program.cs
@@ -15,24 +15,35 @@ namespace ExampleUsage
         /// </summary>
         static void Main(string[] args)
         {
-            var opt = args.Length > 0 ? args[0] : "routing";
+            var opt = args.Length > 0 ? args[0] : "simple";
             const string url = "http://localhost:12345";
 
-            switch (opt)
-            { 
-                case "simple":
-                    using (WebApp.Start<StartupSimple>(url))
-                    {
-                        Console.ReadLine();
-                    }
-                break;
+            try
+            {
+                switch (opt)
+                {
+                    case "simple":
+                        using (WebApp.Start<StartupSimple>(url))
+                        {
+                            Console.ReadLine();
+                        }
+                        break;
 
-                case "routing":
-                    using (WebApp.Start<StartupRouting>(url))
-                    {
-                        Console.ReadLine();
-                    }
-                break;
+                    case "routing":
+                        using (WebApp.Start<StartupRouting>(url))
+                        {
+                            Console.ReadLine();
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                while (ex != null)
+                {
+                    Console.WriteLine(ex.Message);
+                    ex = ex.InnerException;
+                }
             }
         }
     }
