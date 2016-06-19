98bf8a84d009babca24b3e996ee8062ff66ec918
Martin Halliday <martin.bikeman@gmail.com>
2016-06-18 20:54:46 -0700

    Modified the test server to use Urchin for its configuration data.

--- OwinFramework.Middleware/TestServer/Startup.cs
+++ OwinFramework.Middleware/TestServer/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Owin;
+﻿using System;
+using System.IO;
+using Owin;
 using OwinFramework.Builder;
+using OwinFramework.Configuration.Urchin;
 using OwinFramework.RouteVisualizer;
 using OwinFramework.Utility;
 
@@ -9,16 +12,32 @@ namespace TestServer
     {
         public void Configuration(IAppBuilder app)
         {
+            // Craete an Urchin configuration store
+            var configurationStore = new Urchin.Client.Data.ConfigurationStore();
+            configurationStore.Initialize();
+
+            // Tell urchin to get its configuration from the config.json file in this project. Note that if
+            // you edit this file whilst the application is running the changes will be immediately applied
+            // without restarting the application.
+            var fileSource = new Urchin.Client.Sources.FileSource(configurationStore);
+            var configFile = new FileInfo(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "config.json");
+            fileSource.Initialize(configFile, TimeSpan.FromSeconds(10));
+
+            // Tell the Owin Framework to use Urchin for its middleware configuration
+            var configuration = new UrchinConfiguration(configurationStore);
+
+            // Use the Owin Framework  builder to build the Owin pipeline
             var dependencyGraphFactory = new DependencyGraphFactory();
             var segmenterFactory = new SegmenterFactory(dependencyGraphFactory);
-            var configuration = new DefaultValueConfiguration();
             var builder = new Builder(dependencyGraphFactory, segmenterFactory);
 
+            // The route visualizer middleware will produce an SVG showing the Owin pipeline configuration
             builder.Register(new RouteVisualizer())
                 .As("RouteVisualizer")
-                .ConfigureWith(configuration, "/routeVisualizer")
+                .ConfigureWith(configuration, "/middleware/visualizer")
                 .RunFirst();
 
+            // Tell Owin to add our Owin Framework middleware to the Owin pipeline
             app.UseBuilder(builder);
         }
     }
