6564de6760057f3525e4a7f4bb03c5149f4f8cf8
Martin Halliday <martin.bikeman@gmail.com>
2016-11-09 00:48:06 -0800

    Added more Mocks. Not finished yet.

---
+++ OwinFramework.Mocks/Builder/MockConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Moq.Modules;
+using OwinFramework.Interfaces.Builder;
+
+namespace OwinFramework.Mocks.Builder
+{
+    public class MockConfiguration : ConcreteImplementationProvider<IConfiguration>, IConfiguration, IDisposable
+    {
+        protected override IConfiguration GetImplementation(IMockProducer mockProducer)
+        {
+            return this;
+        }
+
+        public IDisposable Register<T>(string path, Action<T> onChangeAction, T defaultValue)
+        {
+            onChangeAction(defaultValue);
+            return this;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
