6ad1142f441cbdd5c2b225426dee89bdec2b2c21
Martin Halliday <martin.bikeman@gmail.com>
2017-01-22 17:27:17 -0800

    Released to NuGet as 2.2.5

--- SharedVersionInfo.cs
+++ SharedVersionInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.2.6")]
-[assembly: AssemblyFileVersion("2.2.6")]
+[assembly: AssemblyVersion("2.2.5")]
+[assembly: AssemblyFileVersion("2.2.5")]
